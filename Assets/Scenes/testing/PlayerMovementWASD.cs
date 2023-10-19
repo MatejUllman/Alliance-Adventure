e67d766fb5eecda88889c4466fab1910a874fd43
MatejUllman <113584839+MatejUllman@users.noreply.github.com>
2023-10-19 10:13:02 +0200

    movement done, camera almost done

---
+++ Assets/Scenes/testing/PlayerMovementWASD.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementWASD : MonoBehaviour
+{
+    //jump
+    [SerializeField] float jumpForce = 5f;
+    [SerializeField] float groundDis = 1.5f;
+
+
+    //Movement
+    [SerializeField] private float speed;
+    [SerializeField] private string inputNameHorizontal;
+    [SerializeField] private string inputNameVertical;
+    [SerializeField] private string inputNameJump;
+
+    [SerializeField] private Color color;
+
+    private Rigidbody rb;
+    private Renderer renderer;
+
+    private float inputHorizontal;
+    private float inputVertical;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        renderer = GetComponentInChildren<Renderer>();
+        renderer.material.color = color;
+    }
+
+    private void Update()
+    {
+        //Movement
+        inputHorizontal = Input.GetAxisRaw(inputNameHorizontal);
+        inputVertical = Input.GetAxisRaw(inputNameVertical);
+
+        //jump
+        if (Input.GetButtonDown(inputNameJump) && isGrounded())
+        {
+            rb.velocity = Vector3.up* jumpForce;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(inputHorizontal * speed * Time.fixedDeltaTime, rb.velocity.y, inputVertical * speed * Time.fixedDeltaTime);
+    }
+    bool isGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, groundDis);
+    }
+}
